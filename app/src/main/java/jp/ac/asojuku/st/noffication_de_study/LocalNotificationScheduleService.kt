35e557ad3f6b940c03eba80e495441856c99567e
ASO1701002 <1701002@st.asojuku.ac.jp>
2019-07-10 16:35:50 +0900

    [UPDATE]四択通知のレイアウトを変更

--- app/src/main/java/jp/ac/asojuku/st/noffication_de_study/LocalNotificationScheduleService.kt
+++ app/src/main/java/jp/ac/asojuku/st/noffication_de_study/LocalNotificationScheduleService.kt
@@ -129,10 +129,10 @@ class LocalNotificationScheduleService : BroadcastReceiver() {
         contentView.setTextViewText(R.id.notifiText, questionText)
 
         contentView.setTextColor(R.id.notifiText, Color.DKGRAY)
-        contentView.setTextColor(R.id.notifyButton, Color.DKGRAY)
-        contentView.setTextColor(R.id.notifyButton2, Color.DKGRAY)
-        contentView.setTextColor(R.id.notifyButton3, Color.DKGRAY)
-        contentView.setTextColor(R.id.notifyButton4, Color.DKGRAY)
+        contentView.setTextColor(R.id.notifyButton, Color.WHITE)
+        contentView.setTextColor(R.id.notifyButton2, Color.WHITE)
+        contentView.setTextColor(R.id.notifyButton3, Color.WHITE)
+        contentView.setTextColor(R.id.notifyButton4, Color.WHITE)
 
 
         // 表示内容を生成
