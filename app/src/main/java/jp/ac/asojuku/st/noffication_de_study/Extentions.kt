24b5eea4a0f19318873c7cd9b917f9cc91ba2161
ASO1701002 <1701002@st.asojuku.ac.jp>
2019-07-19 12:15:21 +0900

    [UPDATE]未使用のメソッドを削除

--- app/src/main/java/jp/ac/asojuku/st/noffication_de_study/Extentions.kt
+++ app/src/main/java/jp/ac/asojuku/st/noffication_de_study/Extentions.kt
@@ -24,22 +24,3 @@ fun <T : View> T.setSafeClickListener(listener: (it: T) -> Unit) {
         listener.invoke(view as T)
     }
 }
-
-/**
- * Extension function for [CompoundButton.setOnCheckedChangeListener].
- * It prevents fast clicking by user.
- */
-@Suppress("UNCHECKED_CAST")
-fun <T : CompoundButton> T.setSafeCheckListener(listener: (it: T, isChecked: Boolean) -> Unit) {
-    setOnCheckedChangeListener { view, _ ->
-        if (view == null) return@setOnCheckedChangeListener
-        view.isEnabled = false
-
-        Handler().postDelayed(
-            { view.isEnabled = true },
-            CLICKABLE_DELAY_TIME
-        )
-
-        listener.invoke(view as T, isChecked)
-    }
-}
